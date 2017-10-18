a6ae45164356f0b28387acb10dec218b3d057518
AnthonySorise <contact@anthonysorise.com>
2017-10-17 23:24:11 -0700

    functional-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table'
 
+const students = [
+    {
+        name: 'Alan',
+        course: 'Astronomy',
+        grade: '95'
+    },
+    {
+        name: 'Bob',
+        course: 'Biology',
+        grade: '85'
+    },
+    {
+        name: 'Chuck',
+        course: 'Chemistry',
+        grade: '75'
+    },
+];
 
+const App = () => {
+    return(
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+};
+export default App;
