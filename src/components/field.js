fd1ce8dc84522db629ff859a5cae88efdf59474d
AnthonySorise <contact@anthonysorise.com>
2017-10-26 11:54:39 -0700

    completed

---
+++ src/components/field.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default (props) =>{
+    return(
+        <div className="form-group">
+            <labe>
+                {props.label}
+            </labe>
+            <input {...props} className="form-control"/>
+        </div>
+    )
+}
