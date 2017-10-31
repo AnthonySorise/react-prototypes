e024b2b2200058d75baf4449c0ad43b6d3a5601b
AnthonySorise <contact@anthonysorise.com>
2017-10-31 14:42:50 -0700

    completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Clock from './clock'
 
 export default (props) => {
     return(
-        <div>
-            <h1>REACT!</h1>
+        <div className="container">
+            <h1 className="text-center">REACT!</h1>
+            <Clock/>
         </div>
     )
 }
